61677699b35bd761ad624c2b53e48b7db4f8aa3f
panjunyong <panjunyong@everydo.com>
2013-07-18 16:45:09 +0800

    version id

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.2'
+__version__ = '0.3'
 
 import os
 from setuptools import setup, find_packages
