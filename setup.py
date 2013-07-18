c4397666dbbe4a0beab1a087741690e57d26966c
panjunyong <panjunyong@everydo.com>
2013-07-18 18:14:23 +0800

    version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.3'
+__version__ = '0.5'
 
 import os
 from setuptools import setup, find_packages
