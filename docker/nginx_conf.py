af6894d34d019e8b7f954a4d0139c7caae916461
paicha <paicha.me@gmail.com>
2014-03-04 15:44:32 +0800

    编写nginx配置脚本&更新默认模板

--- docker/nginx_conf.py
+++ docker/nginx_conf.py
@@ -1,31 +1,49 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 
 """ 根据站点的配置文件，生成nginx的配置 
 
-- input: /var/sites
+- input: /var/sites/contents/sites/_config.yaml
 - output: /etc/nginx/sites-enabled/zcms.conf:
 
-server{
+"""
+
+import os
+import yaml
+
+nginx_conf = """server{
     listen 80;
     location  /  {
-        # uwsgi设置
-        # uwsgi_param SCRIPT_NAME ""; 
-        # include uwsgi_params;
-        # uwsgi_pass 127.0.0.1:9010;
+        proxy_set_header        HOST $host:8000;
 
-        # 设置静态皮肤的访问，也可以改为直接由nginx提供下载
         rewrite ^/themes/(.*) /themes/$1 break;
- 
-        # 通知zcms开启虚拟主机功能
-        # 根站点
-        if ($host = domain.com){
-            proxy_set_header        HOST $host:8001;
-            proxy_set_header        X-ZCMS-VHM true;
-            rewrite ^/(.*) /demo/$1 break;
-        }
- 
-        proxy_pass     http://127.0.0.1:8000;
+
+        proxy_pass     http://127.0.0.1:8001;
         proxy_redirect off;
     }
-}       
+}
+
+%s
 """
+
+if __name__ == '__main__':
+    sites_config = []
+    for site_name in os.listdir('/var/sites/contents/'):
+        config = file('/var/sites/contents/%s/_config.yaml' % site_name, 'r')
+        site_config = yaml.load(config)
+        if site_config['domain_name']:
+            sites_config.append("""server{
+                listen 80;
+                server_name %s;
+                location  /  {
+                proxy_set_header        HOST $host:8000;
+                rewrite ^/themes/(.*) /themes/$1 break;
+                proxy_set_header        X-ZCMS-VHM true;
+
+                rewrite ^/(.*) /%s/$1 break;
+
+                proxy_pass     http://127.0.0.1:8001;
+                proxy_redirect off;
+                }
+                }""" % (site_config['domain_name'], site_name))
+
+    open('/etc/nginx/sites-enabled/zcms.conf', 'w').write(nginx_conf % '\n'.join(sites_config))
